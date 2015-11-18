4d817b1a5d20a7ea593aa078ceaad53bf882d411
Henry Trusiak88 <henry@henry-t.com>
2015-11-18 19:52:46 +1100

    Initial Start

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req,res)
+{
+	res.send('Todo API Root');
+});
+
+app.listen(port, function()
+{
+	console.log('express listening on PORT ' + port);
+});
