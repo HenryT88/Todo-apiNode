0496984d0a2f9135dbe1f57bf03f2a8941f76d8b
Henry Trusiak88 <henry@henry-t.com>
2015-11-23 20:20:58 +1100

    Added Sequelize Sync

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -7,22 +7,72 @@ var sequelize = new Sequelize(undefined, undefined, undefined, {
 //CREATES A TABLE CALLED todos
 var Todo = sequelize.define('todo',{
 	description:{
-		type:Sequelize.STRING
+		type:Sequelize.STRING,
+		allowNull : false,
+		validate: {
+			len: [1,250]
+			
+		}
 	},
 	completed:{
-		type:Sequelize.BOOLEAN
+		type:Sequelize.BOOLEAN,
+		allowNull : false,
+		defaultValue: false
+		
 	}
 })
 
 
-sequelize.sync().then(function(){
+sequelize.sync({
+	//force:true
+}).then(function(){
 	console.log('Everything is Synced');
-	
-	Todo.create({
-		description: 'First Entry',
-		completed: false
-	}).then(function(todo){
-		console.log('Finished');
-		console.log(todo);
+	// Fetch a Todo Item by ID and print to screen - otherwise not Found.
+	Todo.findOne({
+			where: {description: {$like : '%Dog%'}}
+		
+	}).then(function(todoer){
+		
+		if(todoer)
+		{
+			console.log(todoer.toJSON());
+		}
+		else
+		{
+			console.log('Not Found Buddy!!');
+		}
 	});
-});
+});	
+	/*Todo.create({
+		description: 'Take out Trash'
+		
+	}).then(function(todo){
+			return Todo.create({
+				description: 'Walk the Dog'
+		});
+	}).then(function(){
+			//return Todo.findById(1)
+			return Todo.findAll({
+				where:{
+					description: {
+						$like: '%trash%'
+					}
+				}
+				
+			});
+		}).then(function(todos){			
+			if(todos){
+				todos.forEach(function(todo){
+					console.log(todo.toJSON());
+				});
+				
+			}else{
+				console.log('no todo found!!');
+			}
+
+	}).catch(function(e){
+		console.log(e);
+	});
+
+	*/
+
